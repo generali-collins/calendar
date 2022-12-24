dc3005e0bdb603bca06b62418d9a5adb2412c5cd
generali.collins <104697851+generali-collins@users.noreply.github.com>
2022-12-24 14:09:16 +0000

    Add files via upload
    Building a calendar using python

---
+++ calendar1.py
@@ -0,0 +1,17 @@
+def main():
+    n = eval(input('Input the number of days (28-31): '))
+    d = eval(input('Input the starting day(0 = Sun, 1 = Mon,...): '))
+    for j in range(d):
+        print('  ',end=' ')
+    i = 1
+    while i <= n:
+        
+        if i < 10:
+            
+            print('',i,end = ' ')
+        else:
+            print(i,end = ' ')
+        if (i+d)%8 == 0:
+            print(' ')
+        i = i + 1
+main()
